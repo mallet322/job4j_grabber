34aa43e92a465300afc5082dccabc09e61ecab2a
Илья Шокин <mallet322@gmail.com>
2021-11-28 14:51:04 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/com/elias/quartz/SqlRuParse.java
+++ src/main/java/com/elias/quartz/SqlRuParse.java
@@ -1,8 +1,8 @@
 package com.elias.quartz;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import com.elias.grabber.utils.DateTimeParser;
 import com.elias.grabber.utils.SqlRuDateTimeParser;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -12,11 +12,18 @@ import org.jsoup.select.Elements;
 public class SqlRuParse {
 
     public static void main(String[] args) throws Exception {
-        DateTimeParser dateParser = new SqlRuDateTimeParser();
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        List<Element> jobs = row.subList(3, row.size());
-        for (Element td : jobs) {
+        List<String> pages = getPages();
+        for (String url : pages) {
+            Document doc = Jsoup.connect(url).get();
+            Elements rows = doc.select(".postslisttopic");
+            printAllVacancies(rows);
+        }
+    }
+
+    private static void printAllVacancies(List<Element> row) {
+        var dateParser = new SqlRuDateTimeParser();
+        var vacancies = row.subList(3, row.size());
+        for (Element td : vacancies) {
             Element parent = td.parent();
             var job = parent.children().get(1).child(0);
             var jobText = job.text();
@@ -28,6 +35,15 @@ public class SqlRuParse {
             System.out.println("Автор: " + author);
             System.out.println(jobHref);
         }
+    }
+
+    private static List<String> getPages() {
+        var url = "https://www.sql.ru/forum/job-offers/";
+        var pages = new ArrayList<String>();
+        for (int i = 1; i <= 5; i++) {
+            pages.add(url + i);
+        }
+        return pages;
     }
 
 }
